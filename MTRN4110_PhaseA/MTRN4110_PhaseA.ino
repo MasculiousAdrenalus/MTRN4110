8e4c3b601aaf393206b385c94dd3996690b11a94
MasculiousAdrenalus <brycegossling@gmail.com>
2019-06-22 23:32:14 +1000

    still on works with 0, 1

--- MTRN4110_PhaseA/MTRN4110_PhaseA.ino
+++ MTRN4110_PhaseA/MTRN4110_PhaseA.ino
@@ -1,5 +1,3 @@
-// Visual Micro is in vMicro>General>Tutorial Mode
-// 
 /*
     Name:       MTRN4110_PhaseA.ino
     Created:	22/06/2019 7:26:55 PM
@@ -11,22 +9,52 @@
 #include <arduino.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#include <SoftwareSerial.h>
+
+
+//DEFINES
+#define RxD 0
+#define TxD 1
 
 //GLOBALS
+SoftwareSerial BT(RxD, TxD);
+
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 //-------------------------------------------------------------------------------------
 void setup()
 {
+	// Serial Comms
+	Serial.begin(115200);
+	Serial.println("Connection Started");
+	delay(500);
+	BT.begin(115200);
+	BT.print("AT");
+	delay(500);
+	// LCD INIT
 	lcd.init();
 	lcd.backlight();
 	lcd.clear();
-	lcd.print("Testing...");
+	lcd.print("Testing...123");
 
 
 }
+char x, y;
 //-------------------------------------------------------------------------------------
 void loop()
 {
+	if (BT.available()) {
+		x = BT.read();
 
+		Serial.print(x);
+		BT.print(y);
+		lcd.print(x);
+	}
+	if (Serial.available()) {
+		y = Serial.read();
 
+		Serial.print(y);
+		BT.print(y);
+		lcd.print(y);
+	}
+	//delay(2);
 }
