f505314089de871e79dab1a998dd200a14975e2f
MasculiousAdrenalus <brycegossling@gmail.com>
2019-07-15 09:30:15 +1000

    no message

--- Vision/drawSegment.py
+++ Vision/drawSegment.py
@@ -4,20 +4,32 @@ from matplotlib import pyplot as plt
 
 #Read gray image
 img = cv2.imread("maze.jpg",0)
-# img_blur = cv2.bilateralFilter(img,8,150,150)
-img_blur = cv2.GaussianBlur(img, (5, 5), 0)
+
+img_blurBilat = cv2.bilateralFilter(img,8,75,150)
+abs_img_blurBilat = cv2.convertScaleAbs(img_blurBilat)
+
+img_blurG = cv2.GaussianBlur(img, (5, 5), 0)
+abs_img_blurG = cv2.convertScaleAbs(img_blurG)
+
 img_laplacian = cv2.Laplacian(img,cv2.CV_64F)
-abs_dst = cv2.convertScaleAbs(img_laplacian)
+abs_img_laplacian = cv2.convertScaleAbs(img_laplacian)
+
 #Create default parametrization LSD
 lsd = cv2.createLineSegmentDetector(0)
 
 #Detect lines in the image
-lines = lsd.detect(abs_dst)[0] #Position 0 of the returned tuple are the detected lines
+lines = lsd.detect(abs_img_laplacian)[0] #Position 0 of the returned tuple are the detected lines
 
 #Draw detected lines in the image
 drawn_img = lsd.drawSegments(img,lines)
-print(lines[0])
-print(img_laplacian[0])
+for i in range(0,len(lines)):
+    print(lines[i][0])
+
+# print(lines[1](1))
+
+# print(x1)
+# print(y1)
+# print(img_laplacian[0])
 #Show image
 cv2.imshow("LSD",drawn_img )
 cv2.waitKey(0)
