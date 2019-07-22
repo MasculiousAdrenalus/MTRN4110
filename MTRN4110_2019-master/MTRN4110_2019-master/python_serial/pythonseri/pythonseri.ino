9e8f0550ab6b91f9cfe4db8385844f101fd06f15
MasculiousAdrenalus <brycegossling@gmail.com>
2019-07-22 20:12:17 +1000

    no message

--- MTRN4110_2019-master/MTRN4110_2019-master/python_serial/pythonseri/pythonseri.ino
+++ MTRN4110_2019-master/MTRN4110_2019-master/python_serial/pythonseri/pythonseri.ino
@@ -4,11 +4,14 @@
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-char incomingByte = 0;
+byte incomingByte = 0;
 
 void setup() {
     Serial.begin(9600);
     Serial.write("Power On and ");
+    delay(10);
+    Serial3.begin(115200);
+    Serial3.write("Power On and ");
     delay(10);
       // LCD INIT //
   lcd.init();
@@ -25,6 +28,14 @@ void loop()
     //Serial.println("Arduino: ");
     //Serial.write(Serial.read());
     incomingByte = Serial.read();
+    lcd.print(incomingByte);
+//    Serial.flush();
+    }
+    if (Serial3.available() > 0) {
+    //Serial.println("Arduino: ");
+    //Serial.write(Serial.read());
+    incomingByte = Serial3.read();
+    Serial.println(incomingByte);
     lcd.print(incomingByte);
 //    Serial.flush();
     }
